b33146b165a9306880c2d45033ed74b23bc89134
nayanikakalita <84902367+nayanikakalita@users.noreply.github.com>
2021-05-28 15:25:25 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Add
+{
+public static void main(String[] args)
+{
+int a=5;
+int b=7;
+System.out.print("addition=");
+System.out.println(a+b);
+}
+}
---
+++ Dna.java
@@ -0,0 +1,9 @@
+public class Dna
+{
+public static void main(String[] args)
+{
+String dna= "ATACGATACAA";
+
+System.out.println(dna);
+}
+}
